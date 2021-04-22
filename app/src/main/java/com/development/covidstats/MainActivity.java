9273fc4144520af69ba9306e08dd0858f2e76b8a
ondrejvanout <onicu.ov@gmail.com>
2021-04-22 22:48:49 +0200

    Created UI

--- app/src/main/java/com/development/covidstats/MainActivity.java
+++ app/src/main/java/com/development/covidstats/MainActivity.java
@@ -3,12 +3,16 @@ package com.development.covidstats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateButton = findViewById(R.id.date_button);
     }
 }
